e84887d6c7034ee41d0817b31e53af9051c7d516
ANTONELLA YASMÍN SAVELSKI <46288171@ort>
2022-06-30 12:15:31 -0300

    NavBar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header.js'
+import Carrusel from './components/carrusel.js'
+import View from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Carrusel />
+    </div>
+
+  );
+}
+export default App;
